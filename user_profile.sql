08c19c01b1665ac65a4b8adc8b58a8b743256ff5
Sydney Parker <parkersydney21@gmail.com>
2016-12-06 13:45:16 -0500

    assignment 2 edited
    Code to create Database added to sql

--- user_profile.sql
+++ user_profile.sql
@@ -18,6 +18,9 @@ SET time_zone = "+00:00";
 
 --
 -- Database: `user_profile`
+
+CREATE DATABASE IF NOT EXISTS `user_profile` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `user_profile`;
 --
 
 -- --------------------------------------------------------
